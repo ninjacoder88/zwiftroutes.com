dfa956d112420b409addc44e0f91724f25f343c9
Tom Harris <tomharris88@outlook.com>
2022-05-01 20:57:58 -0500

    updated port to 80

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { MongoClient } = require("mongodb");
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.get("/routes", (req, res) => {
     try {
