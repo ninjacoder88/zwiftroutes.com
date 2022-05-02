2b2a90580f319fa946f9497d4870cfbcd04e8444
Tom Harris <tomharris88@outlook.com>
2022-05-01 20:18:29 -0500

    removed console.log

--- src/public/js/pubSub.js
+++ src/public/js/pubSub.js
@@ -3,16 +3,12 @@ const pubsub = (function(){
 
     return {
         subscribe: function(messageName, callback){
-            console.log(`Subscribe ${messageName}`);
-
             if(queues[messageName] === undefined){
                 queues[messageName] = [];
             }
             queues[messageName].push(callback);
         },
         publish: function(messageName, message){
-            console.log(`Publish ${messageName}`);
-
             queues[messageName].forEach(cb => {
                 cb(message);
             });
